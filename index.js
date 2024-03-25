c5a654b53793c37dae15cd5a19f5b42dc92ed61c
Rizwanyaqoob <160166535+Rizwanyaqoob@users.noreply.github.com>
2024-03-25 13:58:52 -0700

    first assignment
    This is my first assignment. simple calculator

---
+++ index.js
@@ -0,0 +1,24 @@
+#! /usr/bin/cnv node
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: "Enter first number", type: "number", name: "Firstnumber" },
+    { message: "Enter second number ", type: "number", name: "secondnumber" },
+    { message: "select one of the operator to perform action",
+        type: "list",
+        name: "operator",
+        choices: ["addition", "subtraction", "multiplication", "division"]
+    },
+]);
+//conditional statement
+if (answer.operator === "addition") {
+    console.log(answer.Firstnumber + answer.secondnumber);
+}
+else if (answer.operator === "subtraction") {
+    console.log(answer.Firstnumber - answer.secondnumber);
+}
+else if (answer.operator === "multiplication") {
+    console.log(answer.Firstnumber * answer.secondnumber);
+}
+else if (answer.operator === "division") {
+    console.log(answer.Firstnumber / answer.secondnumber);
+}
